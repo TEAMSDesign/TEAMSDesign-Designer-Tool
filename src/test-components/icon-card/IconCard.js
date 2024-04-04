6deb0ff9c210319b0d610a47d5ebf1d21f4eae1f
Ariana Rubi <ariana.rubigomez@teamsdesign.com>
2024-04-04 18:00:31 +0200

    correct comment method in material icon

--- src/test-components/icon-card/IconCard.js
+++ src/test-components/icon-card/IconCard.js
@@ -5,7 +5,7 @@ import './IconCard.css';
 function IconCard(props) {
     return <div class="icon-card  icon-card-react">
         <div className='icon-container'>
-            //<OfflineBoltOutlinedIcon className="icon-card__icon" />
+            {/* <OfflineBoltOutlinedIcon className="icon-card__icon" /> */}
         </div>
         <h5>Feature name</h5>
         <p>I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</p>
